b349cbf60d352705be50a912ccc62cd1dede5a83
2017000015JuanEscobar <j.escobar.5ddb@gmail.com>
2026-03-19 19:08:17 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //entrada
+        Console.WriteLine("Numeros positivos o negativos");
+        int num;
+        Console.Write("Dame un número entero:");
+        num = Convert.ToInt32(Console.ReadLine());
+        //proceso
+        //salida
+        if (num >= 0)
+        { 
+            Console.WriteLine("Es un numero positivo");
+        }
+        else
+        {
+            Console.WriteLine("Es un numero negativo");
+        }
+    }
+}
---
+++ numposneg.psc
@@ -0,0 +1,12 @@
+Algoritmo numposneg
+	Escribir "Numeros positivos y negativos"
+	Definir num como Entero
+	Escribir "Dame un numero entero:"
+	leer num
+	Si num >= 0 Entonces
+		Escribir "Es un numero postivo"
+	SiNo
+		Escribir "Es un numero negativo"
+	Fin Si
+	
+FinAlgoritmo
